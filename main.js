1eb14d359a718c58c98a7cc57052121e9757363c
Afsha10 <unityofbeing1@gmail.com>
2023-06-30 23:47:35 +0100

    Project 1: Overview
    1. Linking the JS file
    2. Returning Hello World to the console
    3. Creating our first variable
    4. Concatenating the results
    5. Changing the variable data

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let firstName = 'Florence'
+let lastName = 'Nightingle'
+
+let age = 28
+age = 32
+
+console.log('My name is ' + firstName + ' ' + lastName)
+console.log("I'm " + age + "years old")
+
+
