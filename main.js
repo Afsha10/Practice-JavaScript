111d6253232bdc1e85163d9154c1b21e03d30805
Afsha10 <unityofbeing1@gmail.com>
2023-07-20 22:37:04 +0100

    Project 7: Understanding For Loops
    Practice for loop

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-let grade = prompt("Please enter your grade 0-100") 
-
-if (grade > 100) {
-  alert("Your grade is not valid.");
-} else if (grade >= 90) {
-  alert("Your grade is A.");
-} else if (grade >= 80) {
-  alert("Your grade is B.");
-} else if (grade >= 70) {
-  alert("Your grade is C.");
-} else if (grade >= 60) {
-  alert("Your grade is D.");
-} else if (grade >= 0) {
-  alert("Your grade is F.");
-} else {
-  alert("Your grade is not valid.");
+for (let i = 1; i <= 5; i++) {
+  console.log("Hi " + i)
 }
